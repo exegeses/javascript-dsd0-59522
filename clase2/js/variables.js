b0d016fba3d7b8b46f8bcbf59298789b7b73a1ab
Marcos <exegeses@gmail.com>
2022-07-15 12:06:10 -0300

    variables en javascript

---
+++ clase2/js/variables.js
@@ -0,0 +1,7 @@
+//utilizamos la palabra reservada "let"
+//para crear una variable
+let num = 0; //hardcoding
+//let nombre = 'marcos'; 
+let nombre = prompt('ingrese su nombre');
+document.write( nombre );
+
