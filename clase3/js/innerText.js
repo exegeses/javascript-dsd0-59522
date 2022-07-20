1d8769cb484421270be0f61b964a1506736ab986
Marcos <exegeses@gmail.com>
2022-07-20 11:17:56 -0300

    práctica Quijote (variables)

---
+++ clase3/js/innerText.js
@@ -0,0 +1,8 @@
+//document.write('siempre al final');
+//document.getElementById('txt').innerText = 'texto desde Javascript';
+let txt = document.getElementById('txt');
+
+txt.innerText = 'otro dato';
+//document.getElementById('txt').style.color = '#d00';
+txt.style.color = '#0d0';
+
