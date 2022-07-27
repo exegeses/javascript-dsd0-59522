939ce4c9dc4779a4b219027a22a841585535af0d
Marcos <exegeses@gmail.com>
2022-07-27 10:45:30 -0300

    resolución tarea Mostrar | ocultar  +  material clase 5

--- clase4/js/mostrarOcultar.js
+++ clase4/js/mostrarOcultar.js
@@ -15,3 +15,58 @@ function mostrar()
     //caja.style.visibility = 'visible';
     //caja.style.opacity = '1';
 }
+
+/* propuesta Gus Lineira */
+/*let flag = 1;
+
+function mostrarOcultar()
+{
+    if( flag%2 == 0)
+    {
+        caja.style.display ='block';
+    }
+    else{
+        caja.style.display = 'none';
+    }
+    flag++;
+}*/
+
+
+/*
+* function mostrarOcultar()
+{
+    if( flag == 0)
+    {
+        caja.style.display ='block';
+        flag = 1;
+    }
+    else{
+        caja.style.display = 'none';
+        flag = 0
+    }
+}
+* */
+
+/* propuesta Carlos Camargo */
+/*function mostrarOcultar () {
+    if ( caja.style.display === 'none') {
+        caja.style.display = 'block';
+    } else {
+        caja.style.display = 'none';
+    }
+}
+*/
+
+/* propuesta Jhonny Samir */
+let visible = true;
+function mostrarOcultar(){
+    if ( visible == true ) {
+        ocultar();
+        visible = false;
+    }
+    else
+    {
+        mostrar();
+        visible = true;
+    }
+}
